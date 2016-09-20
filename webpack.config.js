e7db01579c7a2cc89be124943799b3223c906679
Michiel <git@fastfox.nl>
2016-09-20 14:34:56 +0200

    react-hot in loaders result in webpack error, hot realing still works without the line

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './Main.jsx',
-    output: {
-        path: './static',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: ['react-hot', {
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: [
-                    'es2015',
-                    'react',
-                    'react-hmre'
-                ]
-            }
-        }]
-    }
+  entry: './Main.jsx',
+  output: {
+    path: './static',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      //'react-hot', 
+      {
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [
+            'es2015',
+            'react',
+            'react-hmre'
+          ]
+        }
+      }
+    ]
+  }
 };
-
