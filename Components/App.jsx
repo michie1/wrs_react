95c82be4d8754fc518f019082445da1046ecbe9f
Michiel <git@fastfox.nl>
2016-09-20 22:34:48 +0200

    Fix lint errors

--- Components/App.jsx
+++ Components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Details as RiderDetails} from './rider/Details.jsx';
+import { Details as RiderDetails } from './rider/Details.jsx';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,18 +7,18 @@ export default class App extends React.Component {
     this.receiver = 'Michi!el';
     this.rider = {
       name: 'Michiel',
-      licence: 'Elite'
+      licence: 'Elite',
     };
     this.results = [{
       race: {
         name: 'Ronde van de Lier',
       },
-      result: '12'
+      result: '12',
     }, {
       race: {
         name: 'Ronde van de Race',
       },
-      result: '21'
+      result: '21',
     }];
   }
 
@@ -26,7 +26,7 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>Hello!, {this.receiver}!</h1>
-        <RiderDetails rider={this.rider} results={this.results}/>
+        <RiderDetails rider={this.rider} results={this.results} />
       </div>
     );
   }
