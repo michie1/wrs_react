3f2a064c0140f5eeacd4271fb581d50b22094fad
Michiel <git@fastfox.nl>
2016-09-26 20:38:54 +0200

    Use react-router

--- Main.jsx
+++ Main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Router, Route, hashHistory} from 'react-router';
 import App from './Components/App.jsx';
 
+/*
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
+*/
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+), document.getElementById('app'))
