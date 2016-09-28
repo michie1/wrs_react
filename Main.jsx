2b49c1046eb372aa7af7960562de63eef5bf0d97
Michiel <git@fastfox.nl>
2016-09-28 11:37:43 +0200

    Add redux and immutable

--- Main.jsx
+++ Main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './Components/App.jsx';
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { List, Map } from 'immutable';
 
 /*
 ReactDOM.render(
@@ -10,8 +13,39 @@ ReactDOM.render(
 );
 */
 
+let wrsApp = (state, action) => {
+  if (action.type === 'toEnglish') {
+    return state.set(action.index, 'bye');
+  }
+  return state;
+}
+
+let initialState = Map({
+  hoi: 'doei'
+});
+
+let store = createStore(wrsApp, initialState);
+
+console.log(store.getState().get('hoi'));
+store.dispatch({
+  type: 'toEnglish',
+  index: 'hoi'
+});
+
+console.log(store.getState().get('hoi'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
+
+/*
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
   </Router>
 ), document.getElementById('app'))
+*/
